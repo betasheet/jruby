ed59d7833a307d0d43587142dff9d423977ac572
Eric Seckler <eseckler@uci.edu>
2013-06-11 19:39:22 -0700

    jruby.py update: default option ast and yarv-si with array store exception handling eliminated for yarv-si.

--- mvm/jruby.py
+++ mvm/jruby.py
@@ -234,16 +234,16 @@ def run_bmarks_gn(script_path=os.path.abspath(get_maxine_scripts())):
 #          , ("hotspot", "java")
           #("maxine", get_maxine_executable() + '-XX:+TimeCompilation')
           ("maxine", get_maxine_executable())
-          , ("maxine", get_maxine_executable())
+          #, ("maxine", get_maxine_executable())
           #("maxine", get_maxine_executable() + '-XX:+TimeCompilation -C1X:+ExceptionHandlingElimination -C1X:EHEFilter=Lorg/python/core/PyThreadedCodeInterpreter,Lorg/python/core/PyBytecode')
-          #, ("maxine", get_maxine_executable() + '-C1X:+ExceptionHandlingElimination -C1X:EHEFilter=Lorg/python/core/PyThreadedCodeInterpreter,Lorg/python/core/PyBytecode')
+          , ("maxine", get_maxine_executable() + '-C1X:+ExceptionHandlingElimination -C1X:EHEFilter=Lorg/jruby/ast/executable/YARV')
         ]
 
     host_vms= [
-          #("ast", "org.jruby.Main -trun -X-C", "")
-           ("jit", "org.jruby.Main -trun", "")
-          , ("yarv", "org.jruby.Main -tinterpret -X-y", "bytecode")
-          #, ("yarv-si", "org.jruby.Main -tinterpret -X-ysi", "bytecode")
+          ("ast", "org.jruby.Main -trun -X-C", "")
+          # ("jit", "org.jruby.Main -trun", "")
+          #, ("yarv", "org.jruby.Main -tinterpret -X-y", "bytecode")
+          , ("yarv-si", "org.jruby.Main -tinterpret -X-ysi", "bytecode")
         ]
 
     results= []
